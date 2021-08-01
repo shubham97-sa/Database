4f6b6545dd91936cbac4c3cfafbf738016215fcc
sarbaniLandG <sarbanibhowmik@gmail.com>
2021-08-01 22:04:01 +0530

    First Commit

---
+++ Documents/NetBeansProjects/Assignement/src/assignement/year_profit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignement;
+
+/**
+ *
+ * @author SARBANI
+ */
+public class year_profit {
+    public static int year=5;
+    public static int country=0;
+    public static int state=1;
+    public static int city=2;
+    public static int item =4;
+    public static int profit=3;
+    
+    public static void main(String args[]){
+        Assignement.ReadFile();
+      
+    }
+    }
+    
+    
+
